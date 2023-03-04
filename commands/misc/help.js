d412d1b32eb05b83e7ab433202955c2eb33e02ee
matrom01-v2 <matrom01@nmsu.edu>
2023-03-03 17:06:49 -0700

    added basic embeds!

---
+++ commands/misc/help.js
@@ -0,0 +1,48 @@
+const { AttachmentBuilder, EmbedBuilder} = require('discord.js');
+const file = new AttachmentBuilder('./images/catbugpfp.gif');
+
+
+const helperEmbed = new EmbedBuilder({
+    color: 0x007bff,
+    title: 'Helpies',
+    image: {
+        url: 'attachment://catbugpfp.gif',
+    },
+    description: 'Yeah! Everything is okay!',
+    thumbnail: {
+        url: 'https://i.kym-cdn.com/entries/icons/facebook/000/012/812/Catbug_by_sircinnamon-d5riz9k.jpg'
+    },    
+    fields: [
+        {
+            name: 'Commands available',
+            value: 'Here\'s what I can do!'
+        },
+        {
+            name: '/hello',
+            value: 'I say hello!'
+        },
+        {
+            name: '/help',
+            value: 'I help you out!'
+        },
+        {
+            name: '/ban',
+            value: 'Why would you do that?'
+        },
+
+    ],
+    footer: {
+        text: 'Yea but I don\'t know how to make myself go there',
+        icon_url: 'https://www.nicepng.com/png/detail/377-3771581_i-am-catbug-cat-bug-png.png'
+    },
+});
+
+
+module.exports = {
+    name: 'help',
+    description: 'catbug offers you some help!',
+
+    callBack: (client, interaction) => {
+        interaction.reply({embeds: [helperEmbed], files: [file] });
+    }
+}  
