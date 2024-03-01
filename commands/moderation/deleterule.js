c41d7306d7d02365f3480f404cc49983ef1f2ca6
matrom01-v2 <matrom01@nmsu.edu>
2024-03-01 13:04:57 +0900

    created new rule commands for unique servers

---
+++ commands/moderation/deleterule.js
@@ -0,0 +1,91 @@
+const {
+  Client,
+  ActionRowBuilder,
+  ChatInputCommandInteraction,
+  ApplicationCommandOptionType: { Integer },
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} = require("discord.js");
+
+const Rule = require("../../schemas/Rule");
+
+module.exports = {
+  /**
+   *
+   * @param {Client} client
+   * @param {ChatInputCommandInteraction} interaction
+   * @returns
+   */
+  callBack: async (client, interaction) => {
+    const ruleNumberInput = interaction.options.getInteger("rulenum");
+
+    const foundRule = await Rule.findOne({
+      guildId: interaction.guild.id,
+      number: ruleNumberInput,
+    });
+
+    // console.log(`DB found rule: ${foundRule}`);
+
+    if (!foundRule) {
+      interaction.reply(`Hmmm, I don't think that rule exists...`);
+      return;
+    }
+
+    // create confirmation modal
+    const confirmModal = new ModalBuilder()
+      .setTitle("Delete Rule")
+      .setCustomId("deleteRule");
+
+    // confirmation
+    const confirmTextbox = new TextInputBuilder()
+      .setCustomId("confirm")
+      .setRequired(true)
+      .setLabel("Type 'confirm' to delete rule")
+      .setStyle(TextInputStyle.Short);
+
+    const confirmComponent = new ActionRowBuilder().addComponents(
+      confirmTextbox
+    );
+
+    confirmModal.addComponents(confirmComponent);
+
+    await interaction.showModal(confirmModal);
+
+    // time limit submission
+    const submission = await interaction.awaitModalSubmit({
+      time: 240000,
+    });
+
+    if (!submission) {
+      return;
+    }
+
+    const userConfirm = submission.fields.getTextInputValue("confirm");
+
+    if (!userConfirm.toLowerCase() === "confirm") {
+      interaction.reply("no confirm? try again");
+      return;
+    }
+
+    try {
+      await foundRule.deleteOne();
+      console.log(`Rule ${ruleNumberInput} deleted from db!`);
+    } catch (error) {
+      console.log(
+        `Error deleting new rule ${ruleNumberInput} with error: ${error}`
+      );
+    }
+  },
+
+  name: "delete-rule",
+  description: "Catbug purges a rule from the existence!",
+  options: [
+    {
+      name: "rulenum",
+      description: "rule number",
+      required: true,
+      type: Integer,
+    },
+  ],
+};
---
+++ schemas/Rule.js
@@ -0,0 +1,33 @@
+const {Schema, model} = require('mongoose');
+
+const ruleSchema = new Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+
+    guildId: {
+        type: String,
+        required: true,
+    },
+
+    number: {
+        type: String,
+        required: true,
+        default: ''
+    },
+
+    title: {
+        type: String,
+        required: true,
+        default: '',
+    },
+
+    description: {
+        type: String,
+        default: '',
+    },
+
+});
+
+module.exports = model('Rule', ruleSchema);
